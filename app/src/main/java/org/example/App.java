98486bc82d51b1b4dbd59ab7fa463b31233c99e3
Nate <jr75988@mail.harpercollege.edu>
2025-01-28 14:00:07 -0600

    First commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.checkerframework.checker.units.qual.h;
+
 public class App {
   public static void main(String[] args) {
     // Your code goes here
+    HelloWorld helloWorld = new HelloWorld();
+    System.out.println(helloWorld.getGreeting());
   }
 }
